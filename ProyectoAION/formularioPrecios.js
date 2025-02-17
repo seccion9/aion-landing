8bf7372946421d0e8271ff412752d8a8f732b0d2
Darío Gómez Trujillo <gomezbyte@gmail.com>
2025-02-17 20:03:14 +0100

    -
    -

--- ProyectoAION/formularioPrecios.js
+++ ProyectoAION/formularioPrecios.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-document.addEventListener("DOMContentLoaded", function () {
-    const modal = document.getElementById("modal");
-    const cerrarModal = document.querySelector(".cerrar");
-    const botonesPlanes = document.querySelectorAll(".tarjetaPlan .btn");
-    const inputPrecio = document.getElementById("precio");
-
-    // Función para abrir el modal con el precio correspondiente
-    function abrirModal(precio) {
-        inputPrecio.value = precio; 
-        modal.style.display = "flex"; 
-    }
-
-    // Agregamos un evento a cada botón de plan
-    botonesPlanes.forEach(boton => {
-        boton.addEventListener("click", function () {
-            const tarjeta = this.closest(".tarjetaPlan"); 
-            const precio = tarjeta.querySelector(".titulo").textContent;
-            abrirModal(precio);
-        });
-    });
-
-   /* Evento para cerrar el modal al hacer clic en la "X" */
-    cerrarModal.addEventListener("click", function () {
-        modal.style.display = "none";
-    });
-
-    // Evento para cerrar el modal si el usuario hace clic fuera del contenido
-    window.addEventListener("click", function (e) {
-        if (e.target === modal) {
-            modal.style.display = "none";
-        }
-    });
-});
-=======
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("modal");
     const cerrarModal = document.querySelector(".cerrar");
@@ -105,4 +70,3 @@ document.getElementById("formulario").addEventListener("submit", function (event
     });
     
   });
->>>>>>> 18debd553df1b3a9cf20cadae85a93122e43e5f1
