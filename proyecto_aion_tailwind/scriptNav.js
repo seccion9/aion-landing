85c0b68d8ea5e9bdc5ec2669e707e92c5f9a0f19
Darío Gómez Trujillo <143659541+DarioGomez92@users.noreply.github.com>
2025-02-24 23:16:33 +0100

    Delete proyecto_aion_tailwind/scriptNav.js

--- proyecto_aion_tailwind/scriptNav.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    this.classList.toggle('active'); // Agregar o quitar la clase 'active' al botón hamburguesa
-    document.getElementById('nav-list').classList.toggle('active'); // Mostrar u ocultar el menú
-  });
