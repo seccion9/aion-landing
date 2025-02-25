57aaae0fa1dff2d4a80ffc2380b77244e4d941ba
slyder83 <olterg@live.com>
2025-02-25 16:46:14 +0100

    Precios update
    Mayor distancia de altura entre bloques, adición de detalles que mejoras la experiencia

--- proyecto_aion_tailwind/script_tarjetas.js
+++ proyecto_aion_tailwind/script_tarjetas.js
@@ -9,6 +9,8 @@ const precio2Mensual = document.getElementById('precio2Mensual');
 const precio2Anual = document.getElementById('precio2Anual');
 const tipoPlan2 = document.getElementById('tipoPlan2');
 
+const ofertaAnual = document.getElementById('ofertaAnual');
+
 // Función para activar la animación de flip
 function realizarFlip(tarjeta) {
     tarjeta.classList.add('rotate-y-180'); 
@@ -29,21 +31,23 @@ togglePrecio.addEventListener('change', () => {
         if (precio1Mensual.classList.contains('hidden')) {
             precio1Mensual.classList.remove('hidden');
             precio1Anual.classList.add('hidden');
-            tipoPlan1.textContent = "Mensual";
+            tipoPlan1.textContent = "";
         } else {
             precio1Anual.classList.remove('hidden');
             precio1Mensual.classList.add('hidden');
-            tipoPlan1.textContent = "Anual";
+            tipoPlan1.textContent = "Ahorro de 10€/mes";
         }
 
         if (precio2Mensual.classList.contains('hidden')) {
             precio2Mensual.classList.remove('hidden');
             precio2Anual.classList.add('hidden');
-            tipoPlan2.textContent = "Mensual";
+            tipoPlan2.textContent = "";
+            // ofertaAnual.classList.add('hidden');
         } else {
             precio2Anual.classList.remove('hidden');
             precio2Mensual.classList.add('hidden');
-            tipoPlan2.textContent = "Anual";
+            tipoPlan2.textContent = "Ahorro de 15€/mes";
+            // ofertaAnual.classList.remove('hidden');
         }
     }, 0);
 });
