1f9039466dfc3f4b4f22437bc0133a422fbf2358
slyder83 <olterg@live.com>
2025-02-25 12:30:47 +0100

    Funcionabilidad del nav + estilos
    Ahora el menú hamburguesa funciona perfectamente, y además he añadido estilos para que no se solape con las secciones.

---
+++ proyecto_aion_tailwind/menu_hamburguesa.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navList = document.getElementById('nav-list');
+  
+    menuToggle.addEventListener('click', function() {
+      navList.classList.toggle('hidden');
+    });
+  });
+  
