46611c18bd6b9929712e65fd2ae6a9b3f21c34cd
slyder83 <rcenegar@gmail.com>
2025-03-13 14:32:52 +0100

    Update nav.js
    Modificacionn del menu

--- proyecto_aion_tailwind/nav.js
+++ proyecto_aion_tailwind/nav.js
@@ -1,10 +1,10 @@
 function loadNav() {
     const navHTML = `
-    <header class="bg-gray-200 fixed top-0 left-0 w-full h-16 z-[9999] shadow-md">
-        <div class="max-w-screen-xl mx-auto flex items-center h-full px-4">
+    <header class="bg-gray-200 fixed top-0 left-0 w-full h-16 z-50 shadow-md">
+        <div class="max-w-screen-xl mx-auto flex items-center h-full px-4 justify-between">
             <nav class="container mx-auto flex justify-between items-center p-4 relative">
 
-                <!-- contenedor para el logo y el menu -->
+                <!-- Contenedor para el logo y el menu -->
                 <div class="flex justify-between items-center w-full lg:w-auto">
                 
                     <!-- Logo -->
@@ -19,31 +19,34 @@ function loadNav() {
                         </button>
                     </div>
                 </div>
+            
+        
 
-                <!-- Menú de navegación -->
-                <ul id="nav-list" class="hidden lg:flex gap-x-6 absolute top-16 left-0 w-full bg-white lg:bg-transparent lg:static lg:w-auto p-4 lg:p-0 shadow-md lg:shadow-none flex-col lg:flex-row z-50">
-                    <li class="w-full lg:w-auto">
-                        <a href="index.html" class="btn btn-ghost bg-transparent border-0 text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">Inicio</a>
-                    </li>
-                    <li class="w-full lg:w-auto">
-                        <a id="integraciones" href="index.html#carrusel" class="btn btn-ghost bg-transparent border-0 text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">Integraciones</a>
-                    </li>
-                    <li class="w-full lg:w-auto">
-                        <a id="funcional" href="index.html#funcionalidades" class="btn btn-ghost bg-transparent border-0 text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">Funcionalidades</a>
-                    </li>
-                    <li class="w-full lg:w-auto">
-                        <a id="planes" href="index.html#precios" class="btn btn-ghost bg-transparent border-0 text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">Precios</a>
-                    </li>
-                    <li class="w-full lg:w-auto">
-                        <a href="invertir.html" class="btn btn-ghost bg-transparent border-transparent text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">¿Quieres invertir?</a>
-                    </li>
-                    <li class="w-full lg:w-auto">
-                        <a href="formulario.html" id="contacto" class="btn btn-ghost bg-[#ff8000] border-0 text-white hover:bg-orange-700 hover:text-white transition-colors duration-500 ease-in-out w-full flex justify-center">
-                            Contacto
-                        </a>
-                    </li>
-                </ul>            
-            </nav>
+        <!-- Menú de navegación -->
+        <div class="">
+            <ul id="nav-list" class="hidden lg:flex gap-x-6 absolute top-8 right-2 w-64 sm:w-72 md:w-80 bg-white lg:bg-transparent lg:static lg:w-auto p-4 lg:p-0 shadow-md lg:shadow-none flex-col lg:flex-row z-40 rounded-b-md">
+                <li class="w-full lg:w-auto">
+                    <a href="index.html" class="btn btn-ghost bg-transparent border-0 text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">Inicio</a>
+                </li>
+                <li class="w-full lg:w-auto">
+                    <a id="integraciones" href="index.html#carrusel" class="btn btn-ghost bg-transparent border-0 text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">Integraciones</a>
+                </li>
+                <li class="w-full lg:w-auto">
+                    <a id="funcional" href="index.html#funcionalidades" class="btn btn-ghost bg-transparent border-0 text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">Funcionalidades</a>
+                </li>
+                <li class="w-full lg:w-auto">
+                    <a id="planes" href="index.html#precios" class="btn btn-ghost bg-transparent border-0 text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">Precios</a>
+                </li>
+                <li class="w-full lg:w-auto">
+                    <a href="invertir.html" class="btn btn-ghost bg-transparent border-transparent text-black hover:text-orange-600 hover:bg-transparent w-full flex justify-center">¿Quieres invertir?</a>
+                </li>
+                <li class="w-full lg:w-auto">
+                    <a href="formulario.html" id="contacto" class="btn btn-ghost bg-[#ff8000] border-0 text-white hover:bg-orange-700 hover:text-white transition-colors duration-500 ease-in-out w-full flex justify-center">
+                        Contacto
+                    </a>
+                </li>
+            </ul>
+            </nav> 
         </div>
     </header>
     `;
