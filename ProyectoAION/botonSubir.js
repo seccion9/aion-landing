067fd7807a7c3c2c614f46b9eb70182c05ce051f
Dario Gomez <gomezbyte@gmail.com>
2025-03-24 19:25:56 +0100

    Eliminacion de carpetas obsoletas
    Se eliminan todas las variantes anteriores al proyecto dejando solo la carpeta del proyecto final

--- ProyectoAION/botonSubir.js
+++
@@ -1,5 +0,0 @@
-/* Evento que permite el desplazamiento hacia arriba de la página al darle click al boton */
-document.getElementById("botonSubir").addEventListener("click", function () {
-    const nav = document.getElementById("navegador");
-    logo.scrollIntoView({ behavior: "smooth" });
-  });
